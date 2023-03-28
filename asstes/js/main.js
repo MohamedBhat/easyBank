249e93af91f3aaf2041eb4804e6abdcaac5363e8
MohamedBhat <hamadabhat@gmail.com>
2023-03-28 23:00:42 +0200

    first commit

---
+++ asstes/js/main.js
@@ -0,0 +1,56 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+wow = new WOW(
+    {
+    boxClass:     'animate__animated',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+  )
+  wow.init();
+
+
+
+
+
+
+
+
+
+
+
+
+
